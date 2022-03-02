f8a9ee7965fb88f84a5bdca4f07846ad2def9b58
Piper Haywood <piper.haywood@gmail.com>
2022-03-02 13:35:48 -0500

    Improve display of ItemCard within Items

--- web/components/items.js
+++ web/components/items.js
@@ -5,20 +5,22 @@ export default function Items({ items }) {
     <>
       <section className="items">
         {items.map((item) => (
-          <div key={item._id}>
-            <ItemCard
-              title={item.title}
-              mainImage={item.mainImage}
-              slug={item.slug}
-            />
-          </div>
+          (item.slug && item.mainImage && item.mainImage.asset) && (
+            <div key={item._id}>
+              <ItemCard
+                title={item.title}
+                mainImage={item.mainImage}
+                slug={item.slug}
+              />
+            </div>
+          )
         ))}
       </section>
       <style jsx>{`
         .items {
           column-gap: 0.75rem;
           display: grid;
-          grid-template-columns: repeat( auto-fit, minmax(12rem, 1fr) );
+          grid-template-columns: repeat( auto-fit, minmax(13rem, 1fr) );
           row-gap: 0.75rem;
         }
       `}</style>
